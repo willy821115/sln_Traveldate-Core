b5c46cbc6d95386e2b694fdf891f0ebc4e9776fa
冠景 尤 <willy821115@yahoo.com.tw>
2023-08-13 22:06:36 +0800

    新增CTripViewModel

--- prj_Traveldate Core/Controllers/TripController.cs
+++ prj_Traveldate Core/Controllers/TripController.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prj_Traveldate_Core.Models;
 
 namespace prj_Traveldate_Core.Controllers
 {
     public class TripController : Controller
     {
+        private TraveldateContext _db = null;
+
+        public TripController() 
+        {
+        _db=new TraveldateContext();
+            //HttpContext.Session.SetInt32(CDictionary.SK_COMPANYID, 1);
+            //companyID=  (int)HttpContext.Session.GetInt32(CDictionary.SK_COMPANYID);
+        }
+
+
+
         public IActionResult List()
         {
+            
             return View();
         }
         public IActionResult Create()
---
+++ prj_Traveldate Core/ViewModels/CTripViewModel.cs
@@ -0,0 +1,20 @@
+﻿using prj_Traveldate_Core.Models;
+using prj_Traveldate_Core.Models.MyModels;
+
+namespace prj_Traveldate_Core.ViewModels
+{
+    public class CTripViewModel
+    {
+        public List<CTripWrap> cTripWraps { get; set; }
+        public List<CProductWrap> cProductWraps { get; set; }
+
+        public int TripStock(int tripID)
+        {
+            TraveldateContext _db = new TraveldateContext();
+
+            var q = _db.Trips.Where(s => s.TripId == tripID).Select(s => new { orders = s.TripDetails.Count, max = s.MaxNum }).FirstOrDefault();
+            int stock = (int)q.max - (int)q.orders;
+            return stock;
+        }
+    }
+}
