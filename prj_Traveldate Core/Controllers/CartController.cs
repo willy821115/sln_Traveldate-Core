a14f9f5b4b5acb821723d2e1d5ea0a303bf33ff0
TingWu <tingwu0908@gmail.com>
2023-08-16 16:53:12 +0800

    購物車改讀資料庫

--- prj_Traveldate Core/Controllers/CartController.cs
+++ prj_Traveldate Core/Controllers/CartController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prj_Traveldate_Core.Models;
+using prj_Traveldate_Core.Models.MyModels;
+using prj_Traveldate_Core.ViewModels;
 
 namespace prj_Traveldate_Core.Controllers
 {
     public class CartController : Controller
     {
+        int _memberID = 1;
+        TraveldateContext _context;
+        public CartController()
+        {
+            _context = new TraveldateContext();
+        }
         public ActionResult ShoppingCart()
         {
-            return View();
+            CShoppingCartViewModel vm = new CShoppingCartViewModel();
+            vm.cartitems = new List<CCartItem>();
+            vm.cartitems = _context.OrderDetails.Where(c => (c.Order.IsCart == true) && (c.Order.MemberId == _memberID)).Select(c=>
+            new CCartItem
+            {
+                planName = c.Trip.Product.PlanName,
+                date = $"{c.Trip.Date:d}",
+                quantity = c.Quantity,
+                photo = c.Trip.Product.ProductPhotoLists.FirstOrDefault().Photo,
+                unitPrice = c.Trip.UnitPrice
+        }).ToList();
+            vm.recommends = _context.ProductLists.Take(4).ToList();
+            return View(vm);
         }
         [HttpPost]
         public ActionResult ShoppingCart(int id)
---
+++ prj_Traveldate Core/Models/MyModels/CCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace prj_Traveldate_Core.Models.MyModels
+{
+    public class CCartItem
+    {
+        public string? planName { get; set; }
+        public string? date { get; set; }
+        public int? quantity { get; set; }
+        public byte[]? photo { get; set; }
+        public decimal? unitPrice { get; set; }
+    }
+}
