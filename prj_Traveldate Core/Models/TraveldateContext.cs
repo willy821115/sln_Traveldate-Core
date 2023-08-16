ac6ec58578c865fd87454702e9f3f68cf33987a8
Bigbowllll <g2207120@gmail.com>
2023-08-16 08:54:45 +0800

    123

--- prj_Traveldate Core/Models/TraveldateContext.cs
+++ prj_Traveldate Core/Models/TraveldateContext.cs
@@ -79,7 +79,7 @@ public partial class TraveldateContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Traveldate;Integrated Security=True;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=192.168.31.90;Initial Catalog=Traveldate;User ID=tdSQL;Password=1234;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
