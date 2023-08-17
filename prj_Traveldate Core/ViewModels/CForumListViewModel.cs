86066bcbe113fd2d960b7c4d1fe629455cfbf890
pengyazhi <yzyz86031614@gmail.com>
2023-08-17 13:49:35 +0800

    發文的日期autocompelete完成

--- prj_Traveldate Core/ViewModels/CForumListViewModel.cs
+++ prj_Traveldate Core/ViewModels/CForumListViewModel.cs
@@ -12,7 +12,13 @@ namespace prj_Traveldate_Core.ViewModels
         public List<CCountryAndCity> regions { get; set; } =new List<CCountryAndCity>();
         public List<Member>? members { get; set; }
         public List<LevelList>? level { get; set; }
-        public List<ProductPhotoList>? prodPhoto { get; set; }
+        public List<CForumList_prodPhoto>? prodPhoto { get; set; }
         public List<CCategoryAndTags> categories { get; set; } = new List<CCategoryAndTags>();
+        public List<ScheduleList>? schedules { get; set; }  
+    }
+    public class CForumList_prodPhoto
+    {
+        public int prodId { get; set; }
+        public byte[] prodPhoto { get; set; }
     }
 }
