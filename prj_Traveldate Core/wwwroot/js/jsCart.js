4b135b8c27a2ffb11b73983bec8732ae4835a196
ITING WU <tingwu0908@gmail.com>
2023-08-17 11:02:39 +0800

    購物車價格隨數量改變

--- prj_Traveldate Core/wwwroot/js/jsCart.js
+++ prj_Traveldate Core/wwwroot/js/jsCart.js
@@ -31,14 +31,50 @@ $('.cartUnlike').on('click', function () {
     $(this).hide();
     $(this).prev('.cartLike').show();
 });
-$('.cartPlus').on('click', function () {
-    const c = $(this).prev('.count').text();
-    $(this).prev('.count').text(Number(c) + 1);
+
+function calculateTotalPrice() {
+    let totalPrice = 0;
+    $('.itemprice').each(function () {
+        let price = Number($(this).text());
+        totalPrice += price;
+    });
+    $('#totalPrice').text(totalPrice);
+    $('#earnpoint').text("可獲得" + Math.ceil(totalPrice / 100) + "點!")
+}
+
+// 初始化頁面時計算總價
+calculateTotalPrice();
+
+// 點擊 .cartMinus 時
+$('.cartMinus').click(function () {
+    var $itemPrice = $(this).closest('.counter').next('.prices').find('.itemprice');
+    var quantity = Number($(this).next('.count').text());
+    var unitPrice = Number($itemPrice.text()) / quantity;
+
+    if (quantity > 1) {
+        quantity--;
+        $(this).next('.count').text(quantity);
+        $itemPrice.text(unitPrice * quantity);
+    }
+    calculateTotalPrice();
 });
 
-$('.cartMinus').on('click', function () {
-    const c = Number($(this).next('.count').text());
-    if (c > 1) {
-        $(this).next('.count').text(c - 1);
-    }
+// 點擊 .cartPlus 時
+$('.cartPlus').click(function () {
+    var $itemPrice = $(this).closest('.counter').next('.prices').find('.itemprice');
+    var quantity = Number($(this).prev('.count').text());
+    var unitPrice = Number($itemPrice.text()) / quantity;
+
+    quantity++;
+    $(this).prev('.count').text(quantity);
+    $itemPrice.text(unitPrice * quantity);
+    calculateTotalPrice();
 });
+
+$('.cartDele').on('click', function () {
+    $(this).closest('.Cart-Items').fadeOut();
+    calculateTotalPrice();
+})
+
+
+
