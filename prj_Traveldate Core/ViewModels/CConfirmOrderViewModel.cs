8ddf078b490bad18492f1f59c2f73dd16a8986bb
TingWu <tingwu0908@gmail.com>
2023-08-17 12:28:29 +0800

    確認訂單頁面修正

---
+++ prj_Traveldate Core/ViewModels/CConfirmOrderViewModel.cs
@@ -0,0 +1,9 @@
+﻿using prj_Traveldate_Core.Models;
+
+namespace prj_Traveldate_Core.ViewModels
+{
+    public class CConfirmOrderViewModel
+    {
+        public Member member { get; set; }
+    }
+}
