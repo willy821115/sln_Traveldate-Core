e96c1b8cf7f9184bbf6c87c0f7b53f946b3d2096
ITING WU <tingwu0908@gmail.com>
2023-08-17 17:34:12 +0800

    填資料頁連動資料庫(途中)

--- prj_Traveldate Core/ViewModels/CConfirmOrderViewModel.cs
+++ prj_Traveldate Core/ViewModels/CConfirmOrderViewModel.cs
@@ -5,5 +5,8 @@ namespace prj_Traveldate_Core.ViewModels
     public class CConfirmOrderViewModel
     {
         public Member member { get; set; }
+        public List<Companion> companions { get; set; }
+        public List<CouponList> coupons { get; set; }
+        public List<int> checkedItems { get; set; } //orderDetailsID
     }
 }
