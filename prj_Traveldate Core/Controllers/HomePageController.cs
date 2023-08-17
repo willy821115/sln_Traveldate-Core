c9f8c9e72f88c6b85828cd404c84a6d61505f08a
willy821115 <willy821115@yahoo.com.tw>
2023-08-17 18:27:39 +0800

    首頁

--- prj_Traveldate Core/Controllers/HomePageController.cs
+++ prj_Traveldate Core/Controllers/HomePageController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prj_Traveldate_Core.Models;
+using prj_Traveldate_Core.ViewModels;
 
 namespace prj_Traveldate_Core.Controllers
 {
@@ -6,6 +8,36 @@ namespace prj_Traveldate_Core.Controllers
     {
         public IActionResult HomePage()
         {
+            //CHomePresent list = new CHomePresent();
+
+            //TraveldateContext db = new TraveldateContext();
+
+            //var product = from p in  db.ProductLists
+            //              join t in db.Trips on p.ProductId equals t.ProductId
+            //              join o in db.OrderDetails on t.TripId equals o.TripId
+            //              join c in db.CommentLists on p.ProductId equals c.ProductId
+            //              group p by p.ProductId into g
+                          
+
+
+            ////var q = from p in db.Trips
+            ////        orderby p.OrderDetails.Count descending
+            ////        select new { productId = p.Product.ProductId,productName=p.Product.ProductName, unitPrice = p.UnitPrice, commentScore = p.Product.CommentLists.Select(t => t.CommentScore).FirstOrDefault(), ImagePath = p.Product.ProductPhotoLists.Select(t => t.ImagePath).FirstOrDefault() };
+            //List<CHomeViewModel> popularList = new List<CHomeViewModel>();
+            //foreach (var p in q)
+            //{
+            //    CHomeViewModel item = new CHomeViewModel();
+            //    item.productId = p.productId;
+            //    item.productName = p.productName;
+            //    item.ImagePath = p.ImagePath;
+            //    item.unitPrice = (decimal)p.unitPrice;
+            //    item.commentScore = (int)p.commentScoreAverage;
+
+            //    popularList.Add(item);
+            //}
+
+            //list.commentList = popularList;
+
             return View();
         }
 
