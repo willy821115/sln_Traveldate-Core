fb3896e6b4c276fb6fbdac7d5b9f4e11aa636e72
亞芝 彭 <yzyz86031614@gmail.com>
2023-08-21 00:10:45 +0800

    刪除CDictionary中的FILTEREDPRODUCTS_CITY/搜尋頁面複合篩選可以先用標籤及地區篩選後再進行日期篩選及排序,但移除標籤時日期的篩選會被忽略,帶修正

--- prj_Traveldate Core/Controllers/SearchController.cs
+++ prj_Traveldate Core/Controllers/SearchController.cs
@@ -4,12 +4,14 @@ using prj_Traveldate_Core.Models;
 using prj_Traveldate_Core.Models.MyModels;
 using prj_Traveldate_Core.ViewModels;
 using X.PagedList;
+using System.Text.Json;
 using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
 
 namespace prj_Traveldate_Core.Controllers
 {
     public class SearchController : Controller
     {
+        string? json = null;
         CFilteredProductFactory _products = new CFilteredProductFactory();
         CSearchListViewModel _vm = new CSearchListViewModel();
         TraveldateContext _context = null;
@@ -25,20 +27,28 @@ namespace prj_Traveldate_Core.Controllers
             _vm.countryAndCities = _products.qureyFilterCountry();  //商品國家&縣市,左邊篩選列
             _vm.types = _products.qureyFilterTypes();//商品類型,左邊篩選列
 
+            if (!HttpContext.Session.Keys.Contains(CDictionary.SK_FILETREDPRODUCTS_INFO))
+            {
+                 json = JsonSerializer.Serialize(_vm.filterProducts);
+                HttpContext.Session.SetString(CDictionary.SK_FILETREDPRODUCTS_INFO, json);
+            }
             //int pageSize = 5;
             //int pageNumber = page ?? 1;
             //_vm.pages = new PagedList<CFilteredProductItem>(_vm.filterProducts, pageNumber, pageSize);
             //_vm.pages = new StaticPagedList<CFilteredProductItem>(_vm.filterProducts, pageNumber, pageSize, _vm.filterProducts.Count);
             return View(_vm);
         }
-        public IActionResult sortBy(string txtKeyword, string sortType)
+        public IActionResult sortBy(string sortType)
         {
-            _vm.filterProducts = _products.qureyFilterProductsInfo().ToList();
-            if (!string.IsNullOrEmpty(txtKeyword))
+            if (!HttpContext.Session.Keys.Contains(CDictionary.SK_FILETREDPRODUCTS_INFO))
             {
-                _vm.filterProducts = _products.qureyFilterProductsInfo().Where(p => p.productName.Contains(txtKeyword)).ToList();
-                return PartialView(_vm);
+                _vm.filterProducts = _products.qureyFilterProductsInfo().ToList();
+                json = JsonSerializer.Serialize(_vm.filterProducts);
+                HttpContext.Session.SetString(CDictionary.SK_FILETREDPRODUCTS_INFO, json);
             }
+            json = HttpContext.Session.GetString(CDictionary.SK_FILETREDPRODUCTS_INFO);
+            _vm.filterProducts = JsonSerializer.Deserialize<List<CFilteredProductItem>>(json);
+
             if (sortType == "hot")
             {
                 _vm.filterProducts = _vm.filterProducts.OrderByDescending(p => p.orederCount).ToList();//商品cards;
@@ -72,11 +82,45 @@ namespace prj_Traveldate_Core.Controllers
         }
         
         public IActionResult FilterByConditions(List<string> conditions)
-        { 
-            _vm.filterProducts = _products.qureyFilterProductsInfo()
-                .Where(p=>p.productTags.Any(tag=> conditions.Contains(tag))
-                || conditions.Contains(p.city)
-                ).ToList();
+        {
+ 
+            if (conditions.Count >0)
+            {
+                _vm.filterProducts =_products.qureyFilterProductsInfo()
+                                .Where(p => p.productTags.Any(tag => conditions.Contains(tag))
+                                || conditions.Contains(p.city)
+                                ).ToList();
+                json = JsonSerializer.Serialize(_vm.filterProducts);
+                HttpContext.Session.SetString(CDictionary.SK_FILETREDPRODUCTS_INFO, json);
+                return PartialView(_vm);
+            }
+            _vm.filterProducts = _products.qureyFilterProductsInfo().ToList();
+            json = JsonSerializer.Serialize(_vm.filterProducts);
+            HttpContext.Session.SetString(CDictionary.SK_FILETREDPRODUCTS_INFO, json);
+            return PartialView(_vm);
+        }
+        public IActionResult FilterByDate(string startTime,string endTime)
+        {
+            if (!HttpContext.Session.Keys.Contains(CDictionary.SK_FILETREDPRODUCTS_INFO))
+            {
+                _vm.filterProducts = _products.qureyFilterProductsInfo().ToList();
+                json = JsonSerializer.Serialize(_vm.filterProducts);
+                HttpContext.Session.SetString(CDictionary.SK_FILETREDPRODUCTS_INFO, json);
+            }
+
+            json = HttpContext.Session.GetString(CDictionary.SK_FILETREDPRODUCTS_INFO);
+            _vm.filterProducts = JsonSerializer.Deserialize<List<CFilteredProductItem>>(json);
+            if(!string.IsNullOrEmpty(startTime) && !string.IsNullOrEmpty(endTime))
+            {
+                DateTime startDateTime = DateTime.Parse(startTime);
+                DateTime endDateTime = DateTime.Parse(endTime).AddDays(1);
+                _vm.filterProducts = _vm.filterProducts.Where(p=> DateTime.Parse(p.date) > startDateTime && DateTime.Parse(p.date) < endDateTime).ToList();
+                json = JsonSerializer.Serialize(_vm.filterProducts);
+                HttpContext.Session.SetString(CDictionary.SK_FILETREDPRODUCTS_INFO, json);
+                return PartialView(_vm);
+            }
+            //json = JsonSerializer.Serialize(_vm.filterProducts);
+            //HttpContext.Session.SetString(CDictionary.SK_FILETREDPRODUCTS_INFO, json);
             return PartialView(_vm);
         }
     }
