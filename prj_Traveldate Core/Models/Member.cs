dbff120f5ba6f303fb6c09026cd5f4572bf65733
TingWu <tingwu0908@gmail.com>
2023-08-22 14:22:19 +0800

    login&signup

--- prj_Traveldate Core/Models/Member.cs
+++ prj_Traveldate Core/Models/Member.cs
@@ -9,20 +9,22 @@ public partial class Member
     
     public int MemberId { get; set; }
 
-    [DisplayName("姓")]
+    [DisplayName("姓氏")]
     public string? LastName { get; set; }
 
-    [DisplayName("名")]
+    [DisplayName("名字")]
     public string? FirstName { get; set; }
 
     [DisplayName("性別")]
     public string? Gender { get; set; }
 
+    [DisplayName("身分證字號")]
     public string? Idnumber { get; set; }
 
     [DisplayName("出生日期")]
     public DateTime? BirthDate { get; set; }
-
+    
+    [DisplayName("居住地區")]
     public int? CityId { get; set; }
 
     [DisplayName("手機號碼")]
@@ -31,6 +33,7 @@ public partial class Member
     [DisplayName("聯絡信箱")]
     public string? Email { get; set; }
 
+    [DisplayName("密碼")]
     public string? Password { get; set; }
 
     public int? LevelId { get; set; }
@@ -39,6 +42,7 @@ public partial class Member
 
     public bool? Enable { get; set; }
 
+    [DisplayName("大頭照")]
     public byte[]? Photo { get; set; }
 
     //public string? ImagePath { get; set; }
