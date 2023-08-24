6edcd70332957d05b57fcd120dd560e94b992a6b
Bigbowllll <g2207120@gmail.com>
2023-08-24 15:27:09 +0800

    發放優惠券

--- prj_Traveldate Core/ViewModels/CCouponSendViewModel.cs
+++ prj_Traveldate Core/ViewModels/CCouponSendViewModel.cs
@@ -3,8 +3,11 @@
 namespace prj_Traveldate_Core.ViewModels
 {
     public class CCouponSendViewModel
-    {
-        public CouponList Coupon { get; set; }
+    {     
+        public List<CouponList> Coupons { get; set; }
         public List<Member> Members { get; set; }
+        public int SelectedCouponId { get; set; }
+        public List<int> SelectedMemberIds { get; set; }
+
     }
 }
