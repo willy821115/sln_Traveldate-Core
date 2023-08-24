7db82b7c958790684876e03b0c0f6974d9a51b3a
TingWu <tingwu0908@gmail.com>
2023-08-24 11:59:20 +0800

    註冊上傳大頭貼 廠商Session存ID 廠商Supercontroller

--- prj_Traveldate Core/Controllers/LoginController.cs
+++ prj_Traveldate Core/Controllers/LoginController.cs
@@ -5,10 +5,19 @@ using prj_Traveldate_Core.Models.MyModels;
 using prj_Traveldate_Core.ViewModels;
 using System.Text.Json;
 
+//TODO: 登入: 第三方登入 & 記住帳號密碼(cookie) & 確認Enable & 登入後跳轉
+//TODO: 註冊: 欄位名稱 & 必填欄位 & 驗證 & 註冊成功跳轉
+
 namespace prj_Traveldate_Core.Controllers
 {
     public class LoginController : Controller
     {
+        private IWebHostEnvironment _enviro = null;
+
+        public LoginController(IWebHostEnvironment p)
+        {
+            _enviro = p;
+        }
         public ActionResult Login()
         {
             return View();
@@ -16,9 +25,9 @@ namespace prj_Traveldate_Core.Controllers
         [HttpPost]
         public ActionResult Login(CLoginMemberViewModel vm)
         {
-            Member mem= (new TraveldateContext()).Members.FirstOrDefault(
+            Member mem = (new TraveldateContext()).Members.FirstOrDefault(
                 t => t.Phone.Equals(vm.mlUsername) && t.Password.Equals(vm.mlPassword));
-            if ( mem != null && mem.Password.Equals(vm.mlPassword))
+            if (mem != null && mem.Password.Equals(vm.mlPassword))
             {
                 //string json = JsonSerializer.Serialize(mem);
                 HttpContext.Session.SetString(CDictionary.SK_LOGGEDIN_USER, mem.MemberId.ToString());
@@ -41,7 +50,7 @@ namespace prj_Traveldate_Core.Controllers
             if (com != null && com.Password.Equals(vm.clPassword))
             {
                 //string json = JsonSerializer.Serialize(mem);
-                HttpContext.Session.SetString(CDictionary.SK_LOGGEDIN_COMPANY, com.TaxIdNumber.ToString());
+                HttpContext.Session.SetString(CDictionary.SK_LOGGEDIN_COMPANY, com.CompanyId.ToString());
                 return RedirectToAction("List", "Dashboard");
             }
             ViewBag.Message = "帳號或密碼錯誤";
@@ -50,6 +59,29 @@ namespace prj_Traveldate_Core.Controllers
 
         public ActionResult CompanySignUp()
         {
+            TraveldateContext db = new TraveldateContext();
+            List<SelectListItem> country = new List<SelectListItem>();
+            foreach (var c in db.CountryLists)
+            {
+                SelectListItem cnty = new SelectListItem()
+                {
+                    Value = c.Country,
+                    Text = c.Country
+                };
+                country.Add(cnty);
+            }
+            List<SelectListItem> cities = new List<SelectListItem>();
+            foreach (var c in db.CityLists)
+            {
+                SelectListItem city = new SelectListItem()
+                {
+                    Value = c.City,
+                    Text = c.City
+                };
+                cities.Add(city);
+            }
+            ViewBag.City = cities;
+            ViewBag.Country = country;
             return View();
         }
         [HttpPost]
@@ -63,27 +95,43 @@ namespace prj_Traveldate_Core.Controllers
 
         public ActionResult SignUp()
         {
-            List<SelectListItem> cities = new List<SelectListItem>();
+            List<SelectListItem> cts = new List<SelectListItem>();
             TraveldateContext db = new TraveldateContext();
-            foreach(var c in db.CityLists)
+            foreach (var c in db.CityLists)
             {
                 SelectListItem city = new SelectListItem()
                 {
                     Value = c.CityId.ToString(),
                     Text = c.City
                 };
-                cities.Add(city);
+                cts.Add(city);
             }
-            ViewBag.CityId = cities;
+            ViewBag.CityId = cts;
             return View();
         }
         [HttpPost]
-        public ActionResult SignUp(Member m)
+        public ActionResult SignUp(CMemberWrap m)
         {
             TraveldateContext db = new TraveldateContext();
-            db.Members.Add(m);
+
+            //取得使用者上傳檔案的原始檔名
+            var fileOriginName = Path.GetFileName(m.photo.FileName);
+            //取原始檔名中的副檔名
+            var fileExt = Path.GetExtension(fileOriginName);
+            string photoName = Guid.NewGuid().ToString() + fileExt;
+            m.ImagePath = photoName;
+            m.photo.CopyTo(new FileStream(
+                _enviro.WebRootPath + "/images/" + photoName, FileMode.Create));
+
+            m.LevelId = 1;
+            m.Discount = 0;
+            m.Enable = true;
+            db.Members.Add(m.member);
             db.SaveChanges();
             return RedirectToAction("Login", "Login");
         }
+
+
+
     }
 }
---
+++ prj_Traveldate Core/Models/MyModels/CMemberWrap.cs
@@ -0,0 +1,113 @@
+﻿using System.ComponentModel;
+
+namespace prj_Traveldate_Core.Models.MyModels
+{
+    public class CMemberWrap
+    {
+        private Member _member = null;
+        public CMemberWrap()
+        {
+            _member = new Member();
+        }
+        public Member member
+        {
+            get { return _member; }
+            set { _member = value; }
+        }
+        [DisplayName("會員編號")]
+        public int MemberId 
+        {
+            get { return _member.MemberId; }
+            set { _member.MemberId = value; }
+        }
+        [DisplayName("姓氏")]
+        public string? LastName
+        {
+            get { return _member.LastName; }
+            set { _member.LastName = value; }
+        }
+
+        [DisplayName("名字")]
+        public string? FirstName
+        {
+            get { return _member.FirstName; }
+            set { _member.FirstName = value; }
+        }
+
+        [DisplayName("性別")]
+        public string? Gender
+        {
+            get { return _member.Gender; }
+            set { _member.Gender = value; }
+        }
+
+        [DisplayName("身分證字號")]
+        public string? Idnumber
+        {
+            get { return _member.Idnumber; }
+            set { _member.Idnumber = value; }
+        }
+
+        [DisplayName("出生日期")]
+        public DateTime? BirthDate
+        {
+            get { return _member.BirthDate; }
+            set { _member.BirthDate = value; }
+        }
+
+        [DisplayName("居住地")]
+        public int? CityId
+        {
+            get { return _member.CityId; }
+            set { _member.CityId = value; }
+        }
+
+        [DisplayName("聯絡電話")]
+        public string? Phone
+        {
+            get { return _member.Phone; }
+            set { _member.Phone = value; }
+        }
+
+        [DisplayName("E-mail")]
+        public string? Email
+        {
+            get { return _member.Email; }
+            set { _member.Email = value; }
+        }
+
+        [DisplayName("密碼")]
+        public string? Password
+        {
+            get { return _member.Password; }
+            set { _member.Password = value; }
+        }
+
+        public int? LevelId
+        {
+            get { return _member.LevelId; }
+            set { _member.LevelId = value; }
+        }
+
+        public int? Discount
+        {
+            get { return _member.Discount; }
+            set { _member.Discount = value; }
+        }
+
+        public bool? Enable
+        {
+            get { return _member.Enable; }
+            set { _member.Enable = value; }
+        }
+
+        public string? ImagePath
+        {
+            get { return _member.ImagePath; }
+            set { _member.ImagePath = value; }
+        }
+
+        public IFormFile photo { get; set; }
+
+    }
+}
