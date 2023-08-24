28676945b60b5901ea920a20199801925acca5b0
TingWu <tingwu0908@gmail.com>
2023-08-24 16:03:38 +0800

    購物車加入/取消最愛 自購物車刪除 刪除所有已選

--- prj_Traveldate Core/Models/MyModels/CCartItem.cs
+++ prj_Traveldate Core/Models/MyModels/CCartItem.cs
@@ -3,10 +3,13 @@
     public class CCartItem
     {
         public int orderDetailID { get; set; }
+        public int productID { get; set; }
         public string? planName { get; set; }
         public string? date { get; set; }
         public int? quantity { get; set; }
         public byte[]? photo { get; set; }
+        public string? ImagePath { get; set; }
         public decimal? unitPrice { get; set; }
+        public bool favo { get; set; }
     }
 }
