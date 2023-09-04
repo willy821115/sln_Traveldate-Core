0a8b0b9b4056e3c4b3a77a12adf0b6486e019109
aazaqq <133043570+aazaqq@users.noreply.github.com>
2023-09-04 16:22:58 +0800

    update MemberController v3

--- prj_Traveldate Core/Controllers/CForumListViewModel2.cs
+++ prj_Traveldate Core/Controllers/CForumListViewModel2.cs
@@ -1,7 +1,25 @@
-﻿namespace prj_Traveldate_Core.Controllers
+﻿using prj_Traveldate_Core.Models;
+using System.Collections.Generic;
+
+namespace prj_Traveldate_Core.Controllers
 {
+    //金使用的 2023.09.04
     internal class CForumListViewModel2
     {
+        private ForumList _forumList = null;
+        private LikeList _likeList = null;
+
+        public ForumList forumList
+        {
+            get { return _forumList; }
+            set { _forumList = value; }
+        }
+        public LikeList likeList
+        {
+            get { return _likeList; }
+            set { _likeList = value; }
+        }
+
         public int ForumListId { get; set; }
         public string Title { get; set; }
         public DateTime? DueDate { get; set; }
@@ -10,5 +28,13 @@
         public int? Watches { get; set; }
         public string? Content { get; set; }
         public bool? IsPublish { get; internal set; }
+
+        //Models- LikeList
+        public int LikeId { get; set; }
+        public int? MemberId { get; set; }
+        public int? ForumId { get; set; }
+        public bool? IsLike { get; set; }
+        public virtual ForumList? Forum { get; set; }
+        public virtual Member? Member { get; set; }
     }
 }
